c563f2549f313702da7de4ec96f04d849e440495
Fabricio2923 <fabriciojaramillo2923@gmail.com>
2022-05-18 17:29:18 -0500

    [feature] - variables & data types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,36 @@
+//fun main(){
+//    println("Hello")
+//}
+
+//fun main(args: Array<String>){
+//    println(args.contentToString())
+//}
+
+// datos generales que hay
+// boolean (size 1bit) representa false/true
+// byte (size 8bit) -127 to 128
+// char (size 16bit) caracter "a" to "z" "/n"
+// double (size 64bit)
+// float (size 32bit)
+// int (size 32bit) num enteros
+// long (size 64bit)
+// short (size 16bit) none
 fun main(){
-    println("Hello")
+    // val es un tipo de variable que NO SE PUEDE MODIFICAR
+    // val palabra reservada
+    val  dayOfBirth : Int = 20
+    val monthOfBirth : Int = 1
+    println("My birthday is  $dayOfBirth/$monthOfBirth")
+    // ó
+    //println("My birthday is: " + dayOfBirth + "/" + monthOfBirth)
+
+    // para variables que se puede ser alteradas
+    var name: String = "Fabricio"
+    var lastname: String = "Jaramillo"
+    //uppercase sirve para que se ponga en mayus
+    //lowercase sirve para que se ponga en minuscula
+    println("My name is:  ${name.uppercase()}  ${lastname.lowercase()}")
+    println("My name is: " + name + "-" + lastname)
+    name = "Antonio"
+    println("My name is $name $lastname")
 }
