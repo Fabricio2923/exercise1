71936a3f38b5808baf2960bb2079d5a3de270f88
Fabricio2923 <fabriciojaramillo2923@gmail.com>
2022-05-18 17:59:39 -0500

    [feature] - variables, data types, etc

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.*
+
 //fun main(){
 //    println("Hello")
 //}
@@ -33,4 +35,69 @@ fun main(){
     println("My name is: " + name + "-" + lastname)
     name = "Antonio"
     println("My name is $name $lastname")
+
+    // suma
+    println(1+1)
+    // resta
+    println(4-2)
+    // division
+    println(4/2)
+    //modulos
+    println(4%2)
+    //multiplicacion
+    println(2*1)
+    //potencia
+    println(2.0.pow(2))
+
+    //Comparacion e igual operadores
+    //Igual
+    println(2==2)
+    // Desigual
+    println(2!=2)
+    // mayor que
+    println(2 > 2)
+    // Menor que
+    println(2<2)
+    // mayor o igual que
+    println(2>=2)
+    //menos o igual que
+    println(2<=2)
+
+    var anyNumber: Int = 0
+    // operadores de asignacion
+    anyNumber += 10
+    anyNumber -= 2
+    anyNumber *= 2
+    anyNumber /= 2
+    anyNumber %= 2
+    // incrementador y decrementador
+    anyNumber ++
+    anyNumber --
+    println(anyNumber)
+
+    var isTrue:Boolean=true
+    var isFalse:Boolean=false
+    // operadores booleanos logicos
+    // AND
+    println(isTrue && isFalse)
+    // OR
+    println(isTrue || isFalse)
+    // NOT - Tipo invertir si es verdadero lo vuelve a falso
+    println(!isTrue)
+    helloWorld()
+    var myNumber : Int = doubletheValue(2)
+    println(myNumber)
+    favoriteFoodPrompt("Fabricio", "Pizza")
+}
+
+fun helloWorld(){
+    println("Hello World")
+}
+
+fun doubletheValue(numberValue: Int) : Int{
+    return numberValue * 2
+}
+
+fun favoriteFoodPrompt(name: String, favoriteFood: String):Unit{
+    println("$name's favorite food is $favoriteFood")
 }
